2f6bcce04facf3a7f31a725cc91c39bc418b4684
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2022-12-10 04:10:47 +0000

    bookers2[topページレイアウトFinish]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters,if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+  end
 
   def after_sign_out_path_for(resource)
     root_path
@@ -11,7 +15,5 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to:"homes#top"
   devise_for :users
+  get "/homes/about" => "homes#about"
 
   resources :books, only:[:new,:create,:index,:show,:destroy]
-  resources :users, only:[:show,:edit]
+  resources :users, only:[:show,:edit,:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
