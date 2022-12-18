cad2219d50e45cae83e401acfb9c7a6b993280fe
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2022-12-18 13:52:30 +0000

    実装テスト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,42 +5,62 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
     @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book)
+      flash[:notice]="You have creatad book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render:index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
-    @user = current_user
+    @user = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice]="You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render:edit
+    end
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to '/books'
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   private
 
     def book_params
-      params.permit(:title,:body,:user_id)
+      params.require(:book).permit(:title,:body,:user_id,:opinion)
     end
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "/homes/about" => "homes#about", as:"about"
+
+
   root to:"homes#top"
-  get "/homes/about" => "homes#about"
 
   resources :books, only:[:new,:create,:index,:show,:edit,:destroy,:update]
   resources :users, only:[:show,:edit,:index,:update]
